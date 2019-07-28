029b3a84d3a3e4a4f2624114f99232b0be87895c
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-29 00:02:56 +1000

    Add files via upload

---
+++ imageClassification.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 10:30:03 2019
+
+@author: saul
+"""
+
+from keras.datasets import mnist
+from keras import models 
+from keras import layers
+
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+network = models.Sequential()
+#two layer Dertse(fully connected) Deep Learning model. 
+#10 classes softmax
+
+network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+
+#compile network
+network.compile(optimizer='rmsprop',
+                loss ='categorical_crossentropy',
+                metrics=['accuracy'])
