4e670c8ba12a6171e0d1d1536a202ff2caec3d06
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-08-14 00:01:25 +1000

    Add files via upload

--- resnet50_imageClassification_twoClass.py
+++ resnet50_imageClassification_twoClass.py
@@ -177,3 +177,30 @@ plt.grid(True)
 plt.legend(['train','val'],loc=4)
 #print plt.style.available # use bmh, classic,ggplot for big pictures
 plt.style.use(['classic'])
+
+#RUN THIS PART FOR PREDICTION
+model = ResNet50(include_top=True,weights='imagenet')
+model.summary()
+model.layers[-1].get_config()
+img_path = '/home/saul/pythontraining/imageClassification/handwriting2.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+#
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds))
+## print: [[u'n02504458', u'African_elephant']]
+#
+##%%
+model = ResNet50(include_top=False,weights='imagenet')
+model.summary()
+model.layers[-1].get_config()
+img_path = '/home/saul/pythontraining/imageClassification/handwriting2.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted:', preds)
