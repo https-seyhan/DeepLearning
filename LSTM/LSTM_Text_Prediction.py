b359da5014b33e3828fc86b5153c2491d3b98de3
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-11-22 17:58:08 +1100

    Update LSTM_Text_Prediction.py

--- LSTM/LSTM_Text_Prediction.py
+++ LSTM/LSTM_Text_Prediction.py
@@ -60,6 +60,7 @@ REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
 BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
 STOPWORDS = set(stopwords.words('english'))
 
+# NLP Data Cleaning
 def clean_text(text):
     """
         text: a string
