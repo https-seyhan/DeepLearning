695af4dcdb39c81072f2753620ecb3165626eda7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2020-12-11 07:14:24 +1100

    Update resnet50_sy.py

--- ResNet/resnet50_sy.py
+++ ResNet/resnet50_sy.py
@@ -43,7 +43,7 @@ from keras_applications.imagenet_utils import _obtain_input_shape
 from keras.engine.topology import get_source_inputs
 
 
-# import resnet weights
+# import pre-trained resnet weights
 WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
 WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
