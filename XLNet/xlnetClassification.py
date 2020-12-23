b0d4f8a402a482d1e125e05f0e75f91987e2f3fc
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2020-12-23 12:50:42 +1100

    Update xlnetClassification.py

--- XLNet/xlnetClassification.py
+++ XLNet/xlnetClassification.py
@@ -69,8 +69,6 @@ tag2name={tag2idx[key] : key for key in tag2idx.keys()}
 #    Set 3 embedding, token embedding, mask word embedding, segmentation embedding
 #    Split data set into train and validate, then send them to dataloader
 
-
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 n_gpu = torch.cuda.device_count()
 n_gpu
