60454c33591e6a8144edb550c8d091a49ed1332f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-20 11:26:53 +1100

    Update resnet50_imageClassification_twoClass.py

--- ResNet/resnet50_imageClassification_twoClass.py
+++ ResNet/resnet50_imageClassification_twoClass.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os
 import time
+
 from resnet50_sy import ResNet50
 from keras.preprocessing import image
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
