4bbd7c2e600d7b2ffa789a0685fee83e4aefad9b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-20 11:25:57 +1100

    Update vgg16_sy.py

--- ResNet/vgg16_sy.py
+++ ResNet/vgg16_sy.py
@@ -107,6 +107,7 @@ def VGG16(include_top=True, weights='imagenet',
             img_input = Input(tensor=input_tensor, shape=input_shape)
         else:
             img_input = input_tensor
+    
     # Block 1
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
