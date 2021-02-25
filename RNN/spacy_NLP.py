7867afa14d67282368d023e133f63899fa87992d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-26 09:13:26 +1100

    Update spacy_NLP.py

--- RNN/spacy_NLP.py
+++ RNN/spacy_NLP.py
@@ -30,7 +30,6 @@ def tokenize_and_vectorized(dataset):
 	for sample in dataset:
 		tokens = tokenizer.tokenize(sample[1]) #tokize sentences 
 		sample_vecs = []
-		
 		print(tokens)		
 		for token in tokens:
 			try:
