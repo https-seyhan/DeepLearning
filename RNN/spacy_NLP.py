c5a2f93862a84f5ae330fb35cb2e43c3606c8172
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-26 09:10:07 +1100

    Update spacy_NLP.py

--- RNN/spacy_NLP.py
+++ RNN/spacy_NLP.py
@@ -7,7 +7,6 @@ from random import shuffle
 
 nlp = spacy.load('en_core_web_md') #load pre-trained model
 tokenizer = Tokenizer(nlp.vocab) #generate tokenizer
-
 traindata = '/home/saul/deeplearning/aclImdb/train'
 
 def pre_process_data(filepath):
