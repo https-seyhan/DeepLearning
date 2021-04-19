0ed69917c3448368c2700090a4c5cb36a90f7794
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-20 07:09:28 +1000

    Update directoryWalk.py

--- directoryWalk.py
+++ directoryWalk.py
@@ -27,8 +27,7 @@ outputpath = '/home/saul/pythontraining/NLP/'
 enlargesize = 1
 
 def convertImageString(folders):
-    image_to_text_list = [['', '', '']]
-    
+    image_to_text_list = [['', '', '']] 
     image_list = []
     
     print ("Folders ", folders)
