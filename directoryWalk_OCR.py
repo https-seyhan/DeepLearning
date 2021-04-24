e3124534c64469b0b302659545ccb6e3e4945591
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-25 06:43:27 +1000

    Update directoryWalk_OCR.py

--- directoryWalk_OCR.py
+++ directoryWalk_OCR.py
@@ -65,7 +65,7 @@ def convertImageString(folders):
                 img= img.point(lambda x:0 if x < 155 else 255, '1')
                 
                 ocrtext = pytesseract.image_to_string(img)
-             
+           
                 image_to_text_list.append([folders[folder],image.split('/')[-1], ocrtext.encode('utf-8') ])
                 
                 
