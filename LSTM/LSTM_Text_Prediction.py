bb1f985e52662aaadb032d220beddb18108fb4c2
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-06 20:46:32 +1000

    Update LSTM_Text_Prediction.py

--- LSTM/LSTM_Text_Prediction.py
+++ LSTM/LSTM_Text_Prediction.py
@@ -17,7 +17,7 @@ from keras.callbacks import EarlyStopping
 from keras.layers import Dropout
 from nltk.corpus import stopwords
 from nltk import word_tokenize
-STOPWORDS = set(stopwords.words('english'))
+
 from bs4 import BeautifulSoup
 import plotly.graph_objs as go
 import plotly.plotly as py
