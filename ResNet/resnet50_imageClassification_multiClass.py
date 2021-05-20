0938512d8a13db39fe927ff82c4ad5f271639a92
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-21 07:41:21 +1000

    Update resnet50_imageClassification_multiClass.py

--- ResNet/resnet50_imageClassification_multiClass.py
+++ ResNet/resnet50_imageClassification_multiClass.py
@@ -108,7 +108,6 @@ t=time.time()
 hist = custom_resnet_model.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
 print('Training time: %s' % (t - time.time()))
 (loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
-
 print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
 
 ###########################################################################################################################
