0568ee327a28826abc8d29db076b71697d645637
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-02 20:23:52 +1000

    Update recurrent_neural_networksNLP.py

--- RNN/recurrent_neural_networksNLP.py
+++ RNN/recurrent_neural_networksNLP.py
@@ -142,7 +142,7 @@ def pad_trunc (data, maxlen):
 			temp = sample
 		new_data.append(temp)
 	return new_data
-	
+
 if __name__ == '__main__':	
 	dataset = pre_process_data(traindata)
 	endtime = datetime.datetime.now()
