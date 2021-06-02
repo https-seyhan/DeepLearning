aa7ca842c4ec843c88023808e6f65c44a509a54a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-02 20:29:22 +1000

    Update recurrent_neural_networksNLP.py

--- RNN/recurrent_neural_networksNLP.py
+++ RNN/recurrent_neural_networksNLP.py
@@ -88,7 +88,6 @@ def rnnmodel(dataset, vectorised_data, expected):
 	x_train = pad_trunc(x_train, maxlen)
 	x_test = pad_trunc(x_test, maxlen)
 
-	print('Maxlen ', maxlen)
 	print('Embedding dims ', embedding_dims)
 
 	x_train= np.reshape(x_train, (len(x_train), maxlen, embedding_dims))
