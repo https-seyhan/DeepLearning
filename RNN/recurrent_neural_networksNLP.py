e2355558076bffc9affa4b16cd391fc188f8a87f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-10 20:46:32 +1000

    Update recurrent_neural_networksNLP.py

--- RNN/recurrent_neural_networksNLP.py
+++ RNN/recurrent_neural_networksNLP.py
@@ -36,7 +36,7 @@ def pre_process_data(filepath):
 	return dataset
 
 def tokenize_and_vectorized(dataset):
-	vectorized_data = []	
+	vectorized_data = []
 	for sample in dataset:
 		tokens = tokenizer.tokenize(sample[1]) #tokize sentences 
 		sample_vecs = []	
