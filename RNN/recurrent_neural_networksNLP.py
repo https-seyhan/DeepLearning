e0725e38886f844bc902e4fd509c870acd1c8a10
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-10 20:48:56 +1000

    Update recurrent_neural_networksNLP.py

--- RNN/recurrent_neural_networksNLP.py
+++ RNN/recurrent_neural_networksNLP.py
@@ -30,6 +30,7 @@ def pre_process_data(filepath):
 		with open(filename, 'r') as f:
 			dataset.append((pos_label, f.read()))
 	for filename in glob.glob(os.path.join(negative_path, '*.txt')):
+		
 		with open(filename, 'r') as f:
 			dataset.append((neg_label, f.read()))	
 	dataset = sample(dataset, 100) #number of samples to be used
