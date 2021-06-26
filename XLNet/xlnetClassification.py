c57ec6a0d526dc95e645ea04ab36a0f8e2318299
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-26 10:56:04 +1000

    Update xlnetClassification.py

--- XLNet/xlnetClassification.py
+++ XLNet/xlnetClassification.py
@@ -209,7 +209,6 @@ max_grad_norm = 1.0
 num_train_optimization_steps = int( math.ceil(len(tr_inputs) / batch_num) / 1) * epochs
 
 #Set fine tuning method
-
 #Manual optimizer
 
 # True: fine tuning all the layers 
