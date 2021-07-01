a5ac171980f28abd4659d0d5efd725f37ad5181e
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-01 10:51:46 +1000

    Update cnnWork.py

--- CNN/cnnWork.py
+++ CNN/cnnWork.py
@@ -72,7 +72,6 @@ def main(model=None, output_dir=None, n_iter=20, n_texts=2000, init_tok2vec=None
     )
 
     train_data = list(zip(train_texts, [{"cats": cats} for cats in train_cats]))
-
     # get names of other pipes to disable them during training
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "textcat"]
 
