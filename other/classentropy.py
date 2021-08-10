341c11b01fb92caae5213077c300452b4020fea7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-11 09:19:03 +1000

    Update classentropy.py

--- other/classentropy.py
+++ other/classentropy.py
@@ -8,7 +8,7 @@ import math
 
 # calculate Entropy
 class Entropy:
- 
+
     def __init__(self, prain=None, pshine = None, p_rain_g_month = None):
         if prain is None:
             self.prain = []  
