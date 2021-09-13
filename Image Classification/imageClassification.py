a0f23cba5612aee9541d08bb593f4cfaf546d561
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-13 11:56:13 +1000

    Update imageClassification.py

--- Image Classification/imageClassification.py
+++ Image Classification/imageClassification.py
@@ -16,7 +16,6 @@ network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
-
 #compile network
 network.compile(optimizer='rmsprop',
                 loss ='categorical_crossentropy',
