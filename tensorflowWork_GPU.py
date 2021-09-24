1f22ef5427d637cf6e02fe9642b9564e3d21492c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-24 10:11:47 +1000

    Update tensorflowWork_GPU.py

--- tensorflowWork_GPU.py
+++ tensorflowWork_GPU.py
@@ -6,7 +6,6 @@
 # tesnserflow test on GPU and CPU
 
 import tensorflow as tf
-
 # Creates a graph.
 with tf.device('/gpu:0'):
   a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
