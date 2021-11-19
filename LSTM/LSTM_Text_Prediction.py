f4fc7de1a8160801e62e1321fcee8102e8fd1cc0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-20 09:23:44 +1100

    Update LSTM_Text_Prediction.py

--- LSTM/LSTM_Text_Prediction.py
+++ LSTM/LSTM_Text_Prediction.py
@@ -31,6 +31,7 @@ InteractiveShell.ast_node_interactivity = 'all'
 df = pd.read_csv('consumer_complaints_small.csv')
 
 df.Product.value_counts()
+
 # Clean Data
 df.loc[df['Product'] == 'Credit reporting', 'Product'] = 'Credit reporting, credit repair services, or other personal consumer reports'
 df.loc[df['Product'] == 'Credit card', 'Product'] = 'Credit card or prepaid card'
