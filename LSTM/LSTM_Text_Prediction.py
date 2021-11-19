53e7f9ec73e2171bb361e81256fdb7ea9d81e195
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-20 09:19:59 +1100

    Update LSTM_Text_Prediction.py

--- LSTM/LSTM_Text_Prediction.py
+++ LSTM/LSTM_Text_Prediction.py
@@ -130,4 +130,4 @@ padded = pad_sequences(seq, maxlen=MAX_SEQUENCE_LENGTH)
 pred = model.predict(padded)
 
 labels = ['Credit reporting, credit repair services, or other personal consumer reports', 'Debt collection', 'Mortgage', 'Credit card or prepaid card', 'Student loan', 'Bank account or service', 'Checking or savings account', 'Consumer Loan', 'Payday loan, title loan, or personal loan', 'Vehicle loan or lease', 'Money transfer, virtual currency, or money service', 'Money transfers', 'Prepaid card']
-print(pred, labels[np.argmax(pred)])
+print(pred, labels[np.argmax(pred)])df.info()
