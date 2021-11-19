6c6a1ee5b66af909569564944742db10b2355e85
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-20 07:57:10 +1100

    Update Image Classification_jupy.py

--- Image Classification/Image Classification_jupy.py
+++ Image Classification/Image Classification_jupy.py
@@ -46,7 +46,6 @@ network.fit(train_images, train_labels, epochs = 5, batch_size=128)
 test_loss, test_acc = network.evaluate(test_images, test_labels)
 
 # In[11]:
-
 print('test_acc:', test_acc)
 
 
