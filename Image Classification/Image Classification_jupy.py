09ec165ed575585640675904a2f472297975d338
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-20 08:27:23 +1100

    Update Image Classification_jupy.py

--- Image Classification/Image Classification_jupy.py
+++ Image Classification/Image Classification_jupy.py
@@ -31,7 +31,6 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000, 28*28))
 test_images = test_images.astype('float32') / 255
 
-
 # In[8]:
 from keras.utils import to_categorical
 train_labels = to_categorical(train_labels)
