c148cf5e137b8b5ef987a0d74f56a5e726a1c717
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-26 12:58:07 +1100

    Update resnet50_imageClassification_twoClass.py

--- ResNet/resnet50_imageClassification_twoClass.py
+++ ResNet/resnet50_imageClassification_twoClass.py
@@ -187,6 +187,7 @@ img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
+
 #
 preds = model.predict(x)
 print('Predicted:', decode_predictions(preds))
