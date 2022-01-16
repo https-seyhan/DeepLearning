7f7160416f1230475d33e1d40800a60c12180a20
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-01-16 17:17:14 +1100

    Update xlnetClassification.py

--- XLNet/xlnetClassification.py
+++ XLNet/xlnetClassification.py
@@ -117,7 +117,7 @@ for i,sentence in enumerate(sentences):
     input_ids = tokens
     # The mask has 0 for real tokens and 1 for padding tokens. Only real
     # tokens are attended to.
-    
+   
     input_mask = [0] * len(input_ids)
     # Zero-pad up to the sequence length at fornt
     if len(input_ids) < max_len:
