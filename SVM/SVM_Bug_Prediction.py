722be4daa1f1136adbfa198191d9ffbb7d19e607
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-02-26 19:54:13 +1100

    Update SVM_Bug_Prediction.py

--- SVM/SVM_Bug_Prediction.py
+++ SVM/SVM_Bug_Prediction.py
@@ -6,9 +6,6 @@ from sklearn import metrics
 #Import scikit-learn dataset libraryDeepLearning/SVM/
 
 from sklearn import datasets
-
-7
-
 from gensim.models import Word2Vec # The word2vec algorithm uses a neural network model to learn word associations from a large corpus of text
 
 8
