bf4a4d45608b55d8653babbfa425d84b08f760fa
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-02-28 11:37:24 +1100

    Update recurrent_neural_networksNLP.py

--- RNN/recurrent_neural_networksNLP.py
+++ RNN/recurrent_neural_networksNLP.py
@@ -56,7 +56,6 @@ def tok_vec(dataset):
 	for sample in dataset:
 		tokens = tokenizer(sample[1])
 		sample_vecs = []
-
 		for token in tokens:	
 			try:
 				sample_vecs.append(nlp(str(token)).vector)
