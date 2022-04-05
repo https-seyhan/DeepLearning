40c834bd52bbf400466611a18a3d7db79fb2c74c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-06 09:37:51 +1000

    Update LSTM_Text_Prediction.py

--- LSTM/LSTM_Text_Prediction.py
+++ LSTM/LSTM_Text_Prediction.py
@@ -66,7 +66,7 @@ def clean_text(text):
     text = REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
     text = BAD_SYMBOLS_RE.sub('', text) # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
     text = text.replace('x', '')
-#    text = re.sub(r'\W+', '', text)
+#   text = re.sub(r'\W+', '', text)
     text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
     return text
 
