7dfd262c2c5aced1dfea0347ba8f89418263207c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-07 19:02:46 +1000

    Update resnet50_imageClassification_twoClass.py

--- ResNet/resnet50_imageClassification_twoClass.py
+++ ResNet/resnet50_imageClassification_twoClass.py
@@ -24,6 +24,7 @@ print('Input image shape:', x.shape)
 
 # Loading the training data
 #PATH = os.getcwd()
+
 PATH = '/home/saul/pythontraining/imageClassification/Transfer-Learning-in-keras---custom-data-master'
 
 # Define data path
