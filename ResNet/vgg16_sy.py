cb991877ef0f8f19590cccb75e4d63b733a15b98
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-07 16:29:08 +1000

    Update vgg16_sy.py

--- ResNet/vgg16_sy.py
+++ ResNet/vgg16_sy.py
@@ -3,7 +3,6 @@
 
 # Reference:
 - [Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/abs/1409.1556)
-
 '''
 from __future__ import print_function
 import numpy as np
