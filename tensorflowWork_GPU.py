18f4bd7145461f40d90481a74a665fba1bad9fff
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-08 12:12:12 +1000

    Update tensorflowWork_GPU.py

--- tensorflowWork_GPU.py
+++ tensorflowWork_GPU.py
@@ -20,7 +20,7 @@ print(sess.run(c))
 tf.test.is_gpu_available()
 
 # Creates a graph.
-with tf.device('/cpu:0'):
+with tf.device('/cpu:0'): #if cpu
   a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
   b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
