2606c3ece7afc7ed6fe7a486ac241c28aa64b109
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-06-20 12:43:40 +1000

    Add files via upload

---
+++ Generative Adversarial Network.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(256, input_shape=(100,), use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Dense(512))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Dense(28 * 28, activation='tanh'))
+    model.add(layers.Reshape((28, 28, 1)))
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dense(256))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dense(1))
+
+    return model
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, 100])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+EPOCHS = 100
+BATCH_SIZE = 128
+
+generator = make_generator_model()
+discriminator = make_discriminator_model()
+
+for epoch in range(EPOCHS):
+    for batch in dataset:
+        train_step(batch)
+
+    # Generate some sample images after each epoch
+    random_vector_for_generation = tf.random.normal([16, 100])
+    generated_images = generator(random_vector_for_generation, training=False)
+
+    # Save the generated images or display them
+    # ...
+
